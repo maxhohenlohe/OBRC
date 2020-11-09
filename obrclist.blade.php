0a3e25e51d7d18669bdf8146aa7feedd0f3b1306
maxhohenlohe <mhohenlohe@gmail.com>
2020-11-09 11:55:15 -0800

    Update obrclist.blade.php

--- obrclist.blade.php
+++ obrclist.blade.php
@@ -74,70 +74,76 @@
                 </table>
             </div>
             </div>
+        </div>
 
-            <div class="container">
+        <br>
+
+        <div class="card">
+            <div class="card-header"><table width="100%"><tr><td><b>Sales Receipt #:</td></tr></table></div>
+            <div class="card-body">
                 <div class="card">
-                    <div class="card-body">
-                        <table class="table table-striped data-table 0DNISVd9 dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-                            <thead>
-                            <tr role="row">
-                                <th align="center" valign="middle" class="head1 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="# : activate to sort column ascending">Sales #</th>
-                                <th align="center" valign="middle" class="head2 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Product/Service : activate to sort column ascending">Product/Service</th>
-                                <th align="center" valign="middle" class="head3 sorting_desc" tabindex="0" rowspan="1" colspan="1" aria-sort="descending" aria-label="Description : activate to sort column ascending">Description</th>
-                                <th align="center" valign="middle" class="head4 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="QTY : activate to sort column ascending">QTY</th>
-                                <th align="center" valign="middle" class="head5 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Rate : activate to sort column ascending">Rate</th>
-                                <th align="center" valign="middle" class="head6 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Amount : activate to sort column ascending">Amount</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($salesreceipts as $salesreceipt)
-                                @foreach ($salesreceipt->Line as $salesreceiptline)
-                                    <td>{{ $salesreceipt->DocNumber }}</td>
-                                    @if ($salesreceiptline->SalesItemLineDetail)
-                                        <?php
-                                        $totalamount = $salesreceipt->TotalAmt;
-                                        $itemname = $salesreceiptline->SalesItemLineDetail->ItemRef;
-                                        $products = app('Spinen\QuickBooks\Client')->getDataService()->Query("Select * From Item");
+                    <table class="table table-striped data-table 0DNISVd9 dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+                        <thead>
+                        <tr role="row">
+                            <th align="center" valign="middle" class="head1 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="# : activate to sort column ascending">Sales #</th>
+                            <th align="center" valign="middle" class="head2 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Product/Service : activate to sort column ascending">Product/Service</th>
+                            <th align="center" valign="middle" class="head3 sorting_desc" tabindex="0" rowspan="1" colspan="1" aria-sort="descending" aria-label="Description : activate to sort column ascending">Description</th>
+                            <th align="center" valign="middle" class="head4 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="QTY : activate to sort column ascending">QTY</th>
+                            <th align="center" valign="middle" class="head5 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Rate : activate to sort column ascending">Rate</th>
+                            <th align="center" valign="middle" class="head6 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Amount : activate to sort column ascending">Amount</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                                        if ($salesreceiptline->SalesItemLineDetail->ItemRef == '22') {
-                                            $petbottle+= $salesreceiptline->SalesItemLineDetail->Qty * 12;
-                                        }
-                                        if ($salesreceiptline->SalesItemLineDetail->ItemRef == '23') {
-                                            $petbottle+= $salesreceiptline->SalesItemLineDetail->Qty;
-                                        }
-                                        if ($salesreceiptline->SalesItemLineDetail->ItemRef == '24') {
-                                            $glassbottle+= $salesreceiptline->SalesItemLineDetail->Qty;
-                                        }
-                                        ?>
-                                        <td>{{ $salesreceiptline->SalesItemLineDetail->ItemRef }}</td>
+                        @foreach ($salesreceipts as $salesreceipt)
+                            @foreach ($salesreceipt->Line as $salesreceiptline)
+                                <td>{{ $salesreceipt->DocNumber }}</td>
+                                @if ($salesreceiptline->SalesItemLineDetail)
+                                    <?php
+                                    $totalamount = $salesreceipt->TotalAmt;
+                                    $itemname = $salesreceiptline->SalesItemLineDetail->ItemRef;
+                                    $products = app('Spinen\QuickBooks\Client')->getDataService()->Query("Select * From Item");
 
-                                        <td>{{ $salesreceiptline->Description }}</td>
-                                        <td>{{ $salesreceiptline->SalesItemLineDetail->Qty }}</td>
-                                        <td>{{ $salesreceiptline->SalesItemLineDetail->UnitPrice }}</td>
-                                        <td>{{ $salesreceiptline->Amount }}</td>
-                                    @else
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        @endif
-                                        </tr>
-                                        @endforeach
-                                        @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                    if ($salesreceiptline->SalesItemLineDetail->ItemRef == '22') {
+                                        $petbottle+= $salesreceiptline->SalesItemLineDetail->Qty * 12;
+                                    }
+                                    if ($salesreceiptline->SalesItemLineDetail->ItemRef == '23') {
+                                        $petbottle+= $salesreceiptline->SalesItemLineDetail->Qty;
+                                    }
+                                    if ($salesreceiptline->SalesItemLineDetail->ItemRef == '24') {
+                                        $glassbottle+= $salesreceiptline->SalesItemLineDetail->Qty;
+                                    }
+                                    ?>
+                                    <td>{{ $salesreceiptline->SalesItemLineDetail->ItemRef }}</td>
+
+                                    <td>{{ $salesreceiptline->Description }}</td>
+                                    <td>{{ $salesreceiptline->SalesItemLineDetail->Qty }}</td>
+                                    <td>{{ $salesreceiptline->SalesItemLineDetail->UnitPrice }}</td>
+                                    <td>{{ $salesreceiptline->Amount }}</td>
+                                @else
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    @endif
+                                    </tr>
+                                    @endforeach
+                                    @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
             <br>
 
-            <div class="container">
+        <div class="card">
+            <div class="card-header"><table width="100%"><tr><td><b>OBRC Count:</td></tr></table></div>
+            <div class="card-body">
                 <div class="card">
-                    <div class="card-body">
                     <table class="table table-striped data-table 0DNISVd9 dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-                    <thead>
-                    <tr role="row">
+                        <thead>
+                        <tr role="row">
                         <th align="center" valign="middle" class="head2 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="Product/Service : activate to sort column ascending">Month</th>
                         <th align="center" valign="middle" class="head3 sorting_desc" tabindex="0" rowspan="1" colspan="1" aria-sort="descending" aria-label="Description : activate to sort column ascending">Year</th>
                         <th align="center" valign="middle" class="head4 sorting" tabindex="0" rowspan="1" colspan="1" aria-label="QTY : activate to sort column ascending">Glass Count</th>
@@ -146,8 +152,8 @@
                      </tr>
                     </thead>
                     <tbody>
-                        <td></td>
-                        <td></td>
+                        <td>November</td>
+                        <td>2020</td>
                         <td>{{ $glassbottle }}</td>
                         <td>{{ $petbottle }}</td>
                         <td>{{ $glassbottle + $petbottle }}</td>
@@ -156,8 +162,12 @@
                     <br>
                  </div>
             </div>
+
         </div>
+        <div class="card">
             <div class="card-header"><table width="100%"><tr><td><a href="/invoices"/><b>Back To Invoices</b></a></td></table></div>
         </div>
+    </div>
+
     </div>
 @endsection
